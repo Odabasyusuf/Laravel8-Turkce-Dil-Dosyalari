e43c2e25a8d49caa4a99a63c2769c51f10d30325
Yusuf Odabaş <yusuf.odabas.78@gmail.com>
2021-06-10 00:16:41 +0300

    Add files via upload

---
+++ auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Girilmiş olan kullanıcı verileri sistemdekiler ile eşleşmemektedir.',
+    'password' => 'Girilen parola yanlış.',
+    'throttle' => 'Çok fazla oturum açma girişiminde bulundunuz. Lütfen :seconds saniye sonra tekrar deneyiniz.',
+];
---
+++ passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Parolanız sıfırlandı!',
+    'sent' => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
+    'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
+    'token' => 'Parola sıfırlama adresi/kodu geçersiz.',
+    'user' => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
+];
